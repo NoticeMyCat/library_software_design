f34eb6d1cf3c63163f11cc33f623cc8c6f80761e
bdanning <bernhard.danninger@fh-salzburg.ac.at>
2023-10-10 13:27:12 +0200

    Order: changed annotation for product reference from ElementCollection to ManyToMany

--- src/main/java/at/ac/fhsalzburg/swd/spring/model/Order.java
+++ src/main/java/at/ac/fhsalzburg/swd/spring/model/Order.java
@@ -7,7 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -21,8 +23,8 @@ public class Order {
 
     @ManyToOne
     private User customer;
-
-    @ElementCollection
+   
+    @ManyToMany
     private Collection<Product> products;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
